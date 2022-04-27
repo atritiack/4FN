3dc01e9d990df818bc6e8698a6c8bae1b1b954e7
Andre Tritiack <atritiack@gmail.com>
2022-04-26 22:13:24 -0300

    Aula 26/04/2022

---
+++ PYTHON/MENU_00.py
@@ -0,0 +1,18 @@
+N1 = int(input('Digite o primeiro número: '))
+N2 = int(input('Digite o segundo número: '))
+
+x = 0
+
+while x != 5:
+    print('''
+    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Maior
+    [ 4 ] Novos números
+    [ 5 ] Sair do programa
+    ''')
+    print('>>>>>>>> Qual é a sua opção?')
+    x = int(input())
+
+print('Fim do programa! Até breve!')
+
