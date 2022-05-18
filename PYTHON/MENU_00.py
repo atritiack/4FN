6d71961f1aeef378ec222af035189d17a5c6c2ba
André Tritiack <atritiack@gmail.com>
2022-05-17 21:54:46 -0300

    Finalizado o exercício MENU

--- PYTHON/MENU_00.py
+++ PYTHON/MENU_00.py
@@ -18,6 +18,23 @@ while x != 5:
         soma = N1 + N2
         print('A soma {} + {} é {}.'.format(N1, N2, soma))
 
+    elif x == 2:
+        produto = N1 * N2
+        print('O produto {} * {} é {}.'.format(N1, N2, produto))
+
+    elif x == 3:
+        if N1 > N2:
+            maior = N1
+        else:
+            maior = N2
+        print('O maior número é {}.'.format(maior))
+
+    elif x == 4:
+        N1 = int(input('Digite o primeiro número: '))
+        N2 = int(input('Digite o segundo número: '))
+
+    else:
+        print('Opção inválida!!')
 
 print('Fim do programa! Até breve!')
 
